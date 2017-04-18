36b24ab60e06ee984f6df8e6af212b0202a3f8ac
Yash5595 <y.tiwari56@gmail.com>
2017-04-18 18:31:53 +0530

    Add files via upload

--- Neural_Network/NN.py
+++ Neural_Network/NN.py
@@ -18,18 +18,16 @@ out = np.zeros((len(inputs), 26))
 iter1 = 0
 
 outputs = np.genfromtxt('train_labels.csv', delimiter=',')
-
-
 for i in outputs:
     out[iter1][i-1]+=1
     iter1+=1
 
 out_train = out[:2*len(out)/3]
 
-#randomly generating weights
+
 np.random.seed(1)
-w1 = np.random.randn(17, 30)
-w2 = np.random.randn(30, 26)
+W1 = np.random.randn(17, 30)
+W2 = np.random.randn(30, 26)
 
 
 def softmax(x):
@@ -48,12 +46,12 @@ def sigmoidprime(z):
 def train():
     iter2 = 0
     prediff = []
-    for i in range(0, inp_train):
+    for i in inp_train:
         
         a = i.reshape(1, 17)
-        b = out_train[iter1].reshape(1, 17)
-        global w1
-        global w2
+        b = out_train[iter2].reshape(1, 26)
+        global W1
+        global W2
         lamda = 0.001
         learn = 0.005
         iter2 += 1
@@ -65,34 +63,31 @@ def train():
             l2 = softmax(np.dot(l1,w2))
             
             #calculating error in layer 2
-            l2_error = y1 - softmax(np.dot(x2, w2)
-            l2_delta = l2_error*(sigmoidprime(l2)) + (lamda*W2)
+            l2_error = b - l2
+            l2_delta = np.dot(l1.T,l2_error) + (lamda*W2)
             
-            #calculating error in layer 1                        
-            l1_error = l2_delta.dot(syn1.T)
-            l1_delta = l1_error * sigmoidprime(l1) + (lamda*W1)
+            #calculating error in layer 1 
+            l1_error = np.dot(l2_error,W2.T)
+            l1_delta = np.dot(sigmoidprime(l0).T,l1_error)  + (lamda*W1)
             
             #updating weights (backpropagation)                   
-            W2 += l1.T.dot(l2_delta)*learn
-            W1 += l0.T.dot(l1_delta)*learn     
-    print prediff
+            W2 += (l2_delta)*learn
+            W1 += (l1_delta)*learn     
+#     print prediff
 train()
 
-                                    
+
+inputs2 = inputs[length:len(inputs)]
+
 def test(x):
-    x1 = np.dot(x, w1)
-    x2 = reluvec(x1)
-    a = softmax(np.dot(x2, w2))
+    l1 = sigmoid(np.dot(l0, w1))
+    a = softmax(np.dot(l1, w2))
     for a in np.argmax(a, axis = 1):
         print a+1
-test(inputs)
+test(inputs2)
 
 
 inp_test = np.genfromtxt('test_data.csv', delimiter=',')
 inp_test = np.concatenate((inp_test, np.ones((len(inp_test), 1))), axis = 1)
-inp_test.shape
 test(inp_test)
-
-
-
 
